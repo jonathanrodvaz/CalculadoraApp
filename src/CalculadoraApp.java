0bb1e4cb1644b389113103ea76417955ad497135
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-08-27 02:15:01 +0200

    Initial Commit

---
+++ src/CalculadoraApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CalculadoraApp {
+    public static void main(String[] args) {
+        System.out.println("****Aplicacion Calculadora****");
+        System.out.print("Proporciona valor operando1: ");
+        Scanner consola = new Scanner(System.in);
+        var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona valor operando2: ");
+        var operando2= Integer.parseInt(consola.nextLine());
+        var resultado = operando1 + operando2;
+        System.out.println("Resultado: " + resultado);
+
+    }
+}
